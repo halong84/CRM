a286334f33018afeb3bf778c77bec8f552f6be50
quangtung1412 <quangtung1412@gmail.com>
2018-05-29 23:14:16 +0700

    Tung 29052018 2314

---
+++ CRM/GUI_DV/frmDangKyDichVu.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace CRM.GUI_DV
+{
+    partial class frmDangKyDichVu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDangKyDichVu));
+            this.SuspendLayout();
+            // 
+            // frmDangKyDichVu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 536);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmDangKyDichVu";
+            this.Text = "frmDangKyDichVu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
